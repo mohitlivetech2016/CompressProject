e22b17a2e58c46181cd2f3e539cc093c9b646d0b
Android <mohitlivetech2016@gmail.com>
2016-11-17 14:41:15 +0530

    Initial commit

---
+++ app/src/main/java/com/dubuging/compressproject/FactoryMethod/FactoryFirst.java
@@ -0,0 +1,24 @@
+package com.dubuging.compressproject.FactoryMethod;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.dubuging.compressproject.R;
+
+public class FactoryFirst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factory_first);
+
+
+
+
+    }
+
+}
